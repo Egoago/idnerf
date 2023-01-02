889bb8724742620e7fcf4c1f21c3edeb868131b8
Csehi Ágoston István <csehi.agoston1999@gmail.com>
2023-01-02 16:09:03 +0100

    idnerf -> bidnerf

--- idnerf/__init__.py
+++
@@ -1,7 +0,0 @@
-from .base import Frame, History, CameraParameters, Data, load_flags
-from .dataset import load_data
-from .math import twist_transformation
-from .log import save
-from .jaxnerf_wrapper import load_model
-from .optimization import fit
-from .rendering import render_img
